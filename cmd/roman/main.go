6a75431d7c46856fbea04d58423b1ec620fca0f9
n1kno <125561616+n1kno@users.noreply.github.com>
2026-03-16 01:03:32 +0400

    Update main.go

--- cmd/roman/main.go
+++ cmd/roman/main.go
@@ -1,1 +1,44 @@
-dgdfgd
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func romanToArabic(roman string) int {
+	values := map[rune]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	roman = strings.ToUpper(roman)
+	result := 0
+	prevValue := 0
+
+	for i := len(roman) - 1; i >= 0; i-- {
+		currentValue := values[rune(roman[i])]
+		
+		if currentValue < prevValue {
+			result -= currentValue
+		} else {
+			result += currentValue
+		}
+		prevValue = currentValue
+	}
+
+	return result
+}
+
+func main() {
+	examples := []string{"III", "IV", "IX", "LVIII", "MCMXC", "MMXXIV"}
+	
+	for _, roman := range examples {
+		arabic := romanToArabic(roman)
+		fmt.Printf("%s = %d\n", roman, arabic)
+	}
+}
